b4f01e8c8c3ed08d6ea2a1ba5136d8e1fad50cb7
Max Jeffries <jeffries.max@gmail.com>
2017-06-25 17:54:40 +0930

    testing static pages

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170625073746) do
+ActiveRecord::Schema.define(version: 20170625081822) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
